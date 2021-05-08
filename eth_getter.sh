6550ef7298e1e93c58d0affe53f0f091b72f04ab
Student-uok3rsbg ee576 <uok3rsbg@users.isi.deterlab.net>
2021-05-08 10:06:51 -0700

    Made eth-getter configurable.

--- eth_getter.sh
+++ eth_getter.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-function trial() {
-	ip_route=$(ip route get 10.1.5.3)
-	ip_isolate_eth=’(eth[0-9])’
-	[[ $ip_route =~ $ip_isolate_eth ]]
-	ETH=${BASH_REMATCH[1]}
-	echo “$ETH”
-}
+
+ip_route=$(ip route get $1)
+ip_isolate_eth="(eth[0-9])"
+[[ $ip_route =~ $ip_isolate_eth ]]
+ETH=${BASH_REMATCH[1]}
+echo "Found connection at $ETH.\n"
+
